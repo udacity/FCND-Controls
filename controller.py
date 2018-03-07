69f2cd013109fe4f3ae876b57e61a20dc539bdd8
Adrien Perkins <anduril582@gmail.com>
2018-03-06 21:25:15 -0800

    add trajectory control to code given to students

--- controller.py
+++ controller.py
@@ -14,7 +14,7 @@ class NonlinearController(object):
     def __init__(self):
         """Initialize the controller object and control gains"""
         return    
-    
+
     def trajectory_control(self, position_trajectory, yaw_trajectory, time_trajectory, current_time):
         """Generate a commanded position, velocity and yaw based on the trajectory
         
@@ -27,10 +27,43 @@ class NonlinearController(object):
         Returns: tuple (commanded position, commanded velocity, commanded yaw)
                 
         """
-        return (np.array([0.0,0.0,0.0]),np.array([0.0,0.0,0.0]),0.0)
+
+        ind_min = np.argmin(np.abs(np.array(time_trajectory) - current_time))
+        time_ref = time_trajectory[ind_min]
+        
+        
+        if current_time < time_ref:
+            position0 = position_trajectory[ind_min - 1]
+            position1 = position_trajectory[ind_min]
+            
+            time0 = time_trajectory[ind_min - 1]
+            time1 = time_trajectory[ind_min]
+            yaw_cmd = yaw_trajectory[ind_min - 1]
+            
+        else:
+            yaw_cmd = yaw_trajectory[ind_min]
+            if ind_min >= len(position_trajectory) - 1:
+                position0 = position_trajectory[ind_min]
+                position1 = position_trajectory[ind_min]
+                
+                time0 = 0.0
+                time1 = 1.0
+            else:
+
+                position0 = position_trajectory[ind_min]
+                position1 = position_trajectory[ind_min + 1]
+                time0 = time_trajectory[ind_min]
+                time1 = time_trajectory[ind_min + 1]
+            
+        position_cmd = (position1 - position0) * \
+                        (current_time - time0) / (time1 - time0) + position0
+        velocity_cmd = (position1 - position0) / (time1 - time0)
+        
+        
+        return (position_cmd, velocity_cmd, yaw_cmd)
     
     def lateral_position_control(self, local_position_cmd, local_velocity_cmd, local_position, local_velocity,
-                               acceleration_ff = np.array([0.0,0.0])):
+                               acceleration_ff = np.array([0.0, 0.0])):
         """Generate horizontal acceleration commands for the vehicle in the local frame
 
         Args:
@@ -42,7 +75,7 @@ class NonlinearController(object):
             
         Returns: desired vehicle 2D acceleration in the local frame [north, east]
         """
-        return np.array([0.0,0.0])
+        return np.array([0.0, 0.0])
     
     def altitude_control(self, altitude_cmd, vertical_velocity_cmd, altitude, vertical_velocity, attitude, acceleration_ff=0.0):
         """Generate vertical acceleration (thrust) command
@@ -69,7 +102,7 @@ class NonlinearController(object):
             
         Returns: 2-element numpy array, desired rollrate (p) and pitchrate (q) commands in radians/s
         """
-        return np.array([0.0,0.0])
+        return np.array([0.0, 0.0])
     
     def body_rate_control(self, body_rate_cmd, body_rate):
         """ Generate the roll, pitch, yaw moment commands in the body frame
@@ -80,7 +113,7 @@ class NonlinearController(object):
             
         Returns: 3-element numpy array, desired roll moment, pitch moment, and yaw moment commands in Newtons*meters
         """
-        return np.array([0.0,0.0,0.0])
+        return np.array([0.0, 0.0, 0.0])
     
     def yaw_control(self, yaw_cmd, yaw):
         """ Generate the target yawrate
