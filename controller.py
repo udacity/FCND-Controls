4900ab1092acad5294fe183f4e0a89b5ede2e8e8
Adrien Perkins <adrienp@stanford.edu>
2018-03-15 13:47:47 -0700

    update gains for a passing solution

--- controller.py
+++ controller.py
@@ -7,7 +7,7 @@ from frame_utils import euler2RM
 
 DRONE_MASS_KG = 0.5
 GRAVITY = -9.81
-MOI = np.array([0.005,0.005,0.01])
+MOI = np.array([0.005, 0.005, 0.01])
 MAX_THRUST = 10.0
 MAX_TORQUE = 1.0
 
@@ -19,17 +19,17 @@ class NonlinearController(object):
         Kp_pos=6,#12.0,
         Kp_vel=4,#8.0,
         Kp_alt=4.0,
-        Kp_hdot=2.0,
+        Kp_hdot=1.0, #2.0,
         
-        Kp_roll=8,#8,#6.5,
-        Kp_pitch=8,#8,#6.5,
-        Kp_yaw=4.5, #4.5,
+        Kp_roll=7,  #8,#6.5,
+        Kp_pitch=7,  #8,#6.5,
+        Kp_yaw=4.5,  #4.5, #4.5,
         
         Kp_p=20,#10,
         Kp_q=20,#10,
-        Kp_r=10,
+        Kp_r=5, #10,
         
-        max_tilt=0.75,
+        max_tilt=1.0,
         max_ascent_rate=5,
         max_descent_rate=2,
         max_speed=5.0
@@ -124,7 +124,7 @@ class NonlinearController(object):
         velocity_cmd = self.Kp_pos*(local_position_cmd-local_position)
         
         #Limit speed
-        velocity_norm = np.sqrt(velocity_cmd[0]*velocity_cmd[0]+ \
+        velocity_norm = np.sqrt(velocity_cmd[0]*velocity_cmd[0] + \
                                 velocity_cmd[1]*velocity_cmd[1])
         
         if velocity_norm > self.max_speed:
@@ -136,7 +136,7 @@ class NonlinearController(object):
 
         return acceleration_cmd
     
-    def altitude_control(self,altitude_cmd,
+    def altitude_control(self, altitude_cmd,
                          vertical_velocity_cmd,
                          altitude,
                          vertical_velocity,
@@ -154,19 +154,19 @@ class NonlinearController(object):
         Returns: thrust command for the vehicle (+up)
         """
         
-        hdot_cmd = self.Kp_alt*(altitude_cmd-altitude)+vertical_velocity_cmd
+        hdot_cmd = self.Kp_alt * (altitude_cmd - altitude) + vertical_velocity_cmd
         
-        #Limit the ascent/descent rate
-        if(hdot_cmd > self.max_ascent_rate):
+        # Limit the ascent/descent rate
+        if hdot_cmd > self.max_ascent_rate:
             hdot_cmd = self.max_ascent_rate
-        elif(hdot_cmd < -self.max_descent_rate):
+        elif hdot_cmd < -self.max_descent_rate:
             hdot_cmd = -self.max_descent_rate
             
-        acceleration_cmd = DRONE_MASS_KG*acceleration_ff + \
+        acceleration_cmd = DRONE_MASS_KG * acceleration_ff + \
                         self.Kp_hdot*(hdot_cmd - vertical_velocity)
         
-        R33 = np.cos(attitude[0])*np.cos(attitude[1])
-        thrust = acceleration_cmd/R33
+        R33 = np.cos(attitude[0]) * np.cos(attitude[1])
+        thrust = acceleration_cmd / R33
         
         if thrust > MAX_THRUST:
             thrust = MAX_THRUST
@@ -174,7 +174,7 @@ class NonlinearController(object):
             thrust = 0.0
         return thrust
     
-    def roll_pitch_controller(self,acceleration_cmd,attitude,thrust_cmd):
+    def roll_pitch_controller(self, acceleration_cmd, attitude, thrust_cmd):
         """ Generate the rollrate and pitchrate commands in the body frame
         
         Args:
@@ -187,26 +187,27 @@ class NonlinearController(object):
                 pitchrate (q) commands in radians/s
         """
         #Calculate rotation matrix        
-        R = euler2RM(attitude[0],attitude[1],attitude[2])
+        R = euler2RM(attitude[0], attitude[1], attitude[2])
         c_d = thrust_cmd/DRONE_MASS_KG
         
         if thrust_cmd > 0.0:
             target_R13 = -np.clip(acceleration_cmd[0].item()/c_d, -self.max_tilt, self.max_tilt) #-min(max(acceleration_cmd[0].item()/c_d, -self.max_tilt), self.max_tilt)
             target_R23 = -np.clip(acceleration_cmd[1].item()/c_d, -self.max_tilt, self.max_tilt) #-min(max(acceleration_cmd[1].item()/c_d, -self.max_tilt), self.max_tilt)
             
-            p_cmd = (1/R[2,2])* \
-                    (-R[1,0]*self.Kp_roll*(R[0,2]-target_R13) + \
-                     R[0,0]*self.Kp_pitch*(R[1,2]-target_R23))
-            q_cmd = (1/R[2,2])* \
-                    (-R[1,1]*self.Kp_roll*(R[0,2]-target_R13) + \
-                     R[0,1]*self.Kp_pitch*(R[1,2]-target_R23))
-        else: #Otherwise command no rate
+            p_cmd = (1/R[2, 2]) * \
+                    (-R[1, 0] * self.Kp_roll * (R[0, 2]-target_R13) + \
+                     R[0, 0] * self.Kp_pitch * (R[1, 2]-target_R23))
+            q_cmd = (1/R[2, 2]) * \
+                    (-R[1, 1] * self.Kp_roll * (R[0, 2]-target_R13) + \
+                     R[0, 1] * self.Kp_pitch * (R[1, 2]-target_R23))
+        else:  # Otherwise command no rate
+            print("negative thrust command")
             p_cmd = 0.0
             q_cmd = 0.0
             thrust_cmd = 0.0
-        return np.array([p_cmd,q_cmd])
+        return np.array([p_cmd, q_cmd])
     
-    def body_rate_control(self,body_rate_cmd,body_rate):
+    def body_rate_control(self, body_rate_cmd, body_rate):
         """ Generate the roll, pitch, yaw moment commands in the body frame
         
         Args:
@@ -217,15 +218,15 @@ class NonlinearController(object):
         Returns: 3-element numpy array, desired roll moment, pitch moment, and
                 yaw moment commands in Newtons*meters
         """
-        Kp_rate = np.array([self.Kp_p,self.Kp_q,self.Kp_r])
-        rate_error = body_rate_cmd-body_rate
+        Kp_rate = np.array([self.Kp_p, self.Kp_q, self.Kp_r])
+        rate_error = body_rate_cmd - body_rate
         
-        moment_cmd = MOI*np.multiply(Kp_rate,rate_error)
+        moment_cmd = MOI * np.multiply(Kp_rate, rate_error)
         if np.linalg.norm(moment_cmd) > MAX_TORQUE:
             moment_cmd = moment_cmd*MAX_TORQUE/np.linalg.norm(moment_cmd)
         return moment_cmd
     
-    def yaw_control(self,yaw_cmd,yaw):
+    def yaw_control(self, yaw_cmd, yaw):
         """ Generate the target yawrate
         
         Args:
@@ -235,14 +236,14 @@ class NonlinearController(object):
         Returns: target yawrate in radians/sec
         """
         
-        #Ensure the target is within range of 0 to 2*pi
-        yaw_cmd = np.mod(yaw_cmd,2.0*np.pi)
+        # Ensure the target is within range of 0 to 2*pi
+        yaw_cmd = np.mod(yaw_cmd, 2.0*np.pi)
         
-        yaw_error = yaw_cmd-yaw
-        if(yaw_error > np.pi):
-            yaw_error = yaw_error-2.0*np.pi
-        elif(yaw_error<-np.pi):
-            yaw_error = yaw_error+2.0*np.pi
+        yaw_error = yaw_cmd - yaw
+        if yaw_error > np.pi:
+            yaw_error = yaw_error - 2.0*np.pi
+        elif yaw_error < -np.pi:
+            yaw_error = yaw_error + 2.0*np.pi
         
         yawrate_cmd = self.Kp_yaw*yaw_error
         return yawrate_cmd
